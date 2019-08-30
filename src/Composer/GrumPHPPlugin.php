96e7f24cfc851fee866b990fb67be9f8ccade31d
Toon Verwerft <toonverwerft@gmail.com>
2019-08-30 10:27:08 +0200

    Add additional installation info for now

--- src/Composer/GrumPHPPlugin.php
+++ src/Composer/GrumPHPPlugin.php
@@ -12,6 +12,8 @@ class GrumPHPPlugin implements PluginInterface
 {
     public function activate(Composer $composer, IOInterface $io)
     {
-        $io->write('<fg=yellow>GrumPHP shim registered. you\'ll need to activate grumphp manually for now.</fg=yellow>');
+        $io->write('<fg=yellow>GrumPHP shim registered. you\'ll need to activate grumphp manually for now:</fg=yellow>');
+        $io->write('Optionally: <fg=green>./vendor/bin/grumphp configure</fg=green>');
+        $io->write('Init hooks: <fg=green>./vendor/bin/grumphp git:init</fg=green>');
     }
 }
